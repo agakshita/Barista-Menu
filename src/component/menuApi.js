ab2e8776d36c340abf9bd92468210307e5130b08
Akshita Agrawal <agakshita02@gmail.com>
2021-09-01 22:05:29 +0530

    intial commit made

---
+++ src/component/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/Croissant.jpg",
+      name: "croissant",
+      category: "quick bites",
+      price: "120₹",
+      description:
+        "A laminated dough rolled Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lorem metus, egestas quis tortor ut, auctor facilisis tortor. Nunc nec efficitur lorem, sit amet dictum leo. Proin nec nisi sodales, ornare tortor a, feugiat sapien.  ",
+    },
+  
+    {
+      id: 2,
+      image: "images/achari-chicken.jpg",
+      name: "achari chicken",
+      category: "rolls",
+      price: "200₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "./images/Achari-Paneer.jpg",
+      name: "achari paneer",
+      category: "rolls",
+      price: "160₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "./images/barista-puff-vegetariano.jpg",
+      name: "vegetariano puff",
+      category: "quick bites",
+      price: "100₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "./images/Chicken-Biryani.jpg",
+      name: "chicken biryani",
+      category: "biryani",
+      price: "400₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "./images/chicken-puff.jpg",
+      name: "chicken puff",
+      category: "quick bites",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "./images/Smoked-Chicken-Sandwich.jpg",
+      name: "smoked chicken sandwich",
+      category: "sandwiches",
+      price: "260₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "./images/Spinach-Corn-Sandwich.jpg",
+      name: "spinach and corn sandwich",
+      category: "sandwiches",
+      price: "240₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "images/veg-biryani.jpg",
+      name: "veg biryani",
+      category: "biryani",
+      price: "300₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  ];
+  
+  export default Menu;
